ec4cb10492c0fb983f66ab5897b5da9660b51706
40-Muhammad Ismail <m.ismail.26934@gmail.com>
2026-05-04 15:19:15 +0800

    Update conn.php

--- conn/conn.php
+++ conn/conn.php
@@ -1,15 +1,13 @@
 <?php
-  $sql = [
-      "user" => "root",
-      "pass" => "",
-      "db" => "db_tools",
-      "host" => "localhost",
-  ];
+$sql = [
+    "user" => "root",
+    "pass" => "",
+    "db" => "db_tools",
+    "host" => "localhost",
+];
 
-  $user = $sql["user"];
-  $pass = $sql["pass"];
-  $db = $sql["db"];
-  $host = $sql["host"];
-
-
+$user = $sql["user"];
+$pass = $sql["pass"];
+$db = $sql["db"];
+$host = $sql["host"];
 ?>
